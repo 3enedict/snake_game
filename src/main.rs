90d5140aa9364b460c1b8090b88526d3d7b44463
BPN06 <benoit.philip.poitevin@gmail.com>
2021-11-01 15:04:02 +0100

    Made the main loop in the main function (logical....)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-mod vulkan;
-use vulkan::Vulkan;
+mod renderer;
+//use renderer::{Renderer, RenderOptions};
+use renderer::Renderer;
+use winit::event_loop::EventLoop;
 
 fn main() {
-    let mut vulkan = Vulkan::init();
+    let mut event_loop = EventLoop::new();
 
-    vulkan.setup(&mut move | vulkan | {
-        println!("Hello world");
-    });
+    /*
+    let mut renderer = Renderer::init(
+        RenderOptions {
+            title: "Snake Game",
+            ..RenderOptions::default()
+        },
+    );
+    */
 
-    vulkan.run();
+    let mut renderer = Renderer::init()
+        .setup(&mut event_loop);
+
+    loop {
+        let exit = renderer.handle_events(&mut event_loop);
+        renderer.run();
+
+        if exit == true {
+            break;
+        }
+    }
 }
