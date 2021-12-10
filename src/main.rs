e8c00faf5d00909b52a4026f71e5e31c624f77f3
BPN06 <benoit.philip.poitevin@gmail.com>
2021-12-10 22:00:22 +0100

    Restarting from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
-mod renderer;
-//use renderer::{Renderer, RenderOptions};
-use renderer::Renderer;
-use winit::event_loop::EventLoop;
+use vgl::start;
 
 fn main() {
-    let mut event_loop = EventLoop::new();
-
-    /*
-    let mut renderer = Renderer::init(
-        RenderOptions {
-            title: "Snake Game",
-            ..RenderOptions::default()
-        },
-    );
-    */
-
-    let mut renderer = Renderer::init()
-        .setup(&mut event_loop);
-
-    loop {
-        let exit = renderer.handle_events(&mut event_loop);
-        renderer.run();
-
-        if exit == true {
-            break;
-        }
-    }
+    start();
 }
