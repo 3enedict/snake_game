4f31fe9c70b27cefc5f26d1e114c7e34af8093c9
BPN06 <benoit.philip.poitevin@gmail.com>
2021-12-20 10:29:08 +0100

    Refactored triangle creation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use vgl::renderer::VglRenderer;
 
-fn main() {
-    let renderer = VglRenderer::new();
+use vgl::objects::triangle::VglTriangle;
+use vgl::objects::vertex::Vertex;
 
-    renderer.run();
+fn system(renderer: &mut VglRenderer) {
+    let triangle = VglTriangle::new(
+        [
+        Vertex { position: [ 0.0, -0.5] },
+        Vertex { position: [ 0.5,  0.5] },
+        Vertex { position: [-0.5,  0.5] },
+        ],
+    );
+
+    renderer.add_triangle(triangle);
+}
+
+fn main() {
+    VglRenderer::new()
+        .add_system_setup(system)
+        .run();
 }
