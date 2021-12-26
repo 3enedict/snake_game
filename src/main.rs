6efd66586a3364b22dbf6137e311df0e14dffbf0
BPN06 <benoit.philip.poitevin@gmail.com>
2021-12-26 21:47:59 +0100

    Added rectangles. Hooraygit status

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ use vgl::renderer::core::parameters::VglRendererParameters;
 use vgl::objects::vertex::Vertex;
 
 fn system(renderer: &mut VglRenderer) {
-    let mut triangles = vec!
+    let mut rectangle = vec!
         [
-        Vertex { position: [ 0.55, -0.5 ] },
-        Vertex { position: [ 0.55,  0.55] },
-        Vertex { position: [-0.5 ,  0.55] },
-
-        Vertex { position: [-0.55,  0.5 ] },
-        Vertex { position: [-0.55, -0.55] },
-        Vertex { position: [ 0.5 , -0.55] },
+        Vertex{ position: [-0.5, -0.5] },
+        Vertex{ position: [ 0.5, -0.5] },
+        Vertex{ position: [ 0.5,  0.5] },
+        Vertex{ position: [-0.5,  0.5] },
         ];
 
-    renderer.add_triangles(&mut triangles);
+    let mut indices = vec![0, 1, 2, 2, 3, 0];
+
+    renderer.add_rectangles(&mut rectangle, &mut indices);
 }
 
 fn main() {
